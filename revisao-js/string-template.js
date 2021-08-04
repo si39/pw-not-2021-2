90f9ed5bb23f6c432c4a9ab16d75bc6e1427f1d9
si39 <sijuta01@gmail.com>
2021-08-04 03:23:31 +0000

    (03/08) Início da revisão de JS)

---
+++ revisao-js/string-template.js
@@ -0,0 +1,15 @@
+let nome = 'Jucicleison'
+let idade = 19
+let cidade = `Franca/SP`
+
+console.log('Meu nome é ' + nome +' tenho ' +  idade + ' anos e moro em ' + cidade + '. ')
+
+//usando string-template ( string-template que é o sinal de crase)
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`)
+
+// String template só funciona quando delimitado por ``
+console.log(`Daqui a cinco anos, ${nome.toUppperCase()} terá ${idade + 5} anos.`)
+
+
+
+
